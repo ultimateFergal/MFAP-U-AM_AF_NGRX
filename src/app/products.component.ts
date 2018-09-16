e4f15e7db740a5299da766ee3753b99a9bbfefa6
Fernando <fernando.galarcio@gmail.com>
2018-09-16 14:31:24 -0500

    new try

---
+++ src/app/products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-products',
+    templateUrl: './products.component.html'
+})
+
+export class ProductsComponent {
+
+    productName = 'A book';
+}
